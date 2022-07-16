3ce10d7e0aacf6da9412d6f93f4e989c577a76de
skand sisodia <sisodiaskand@gmail.com>
2022-07-16 15:09:52 +0530

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,47 +1,47 @@
-import React,{ useState }  from 'react';
-import {useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login(props) {
-    const [cerdential, setCerdential] = useState({email:"",password:""});
-    let navigate=useNavigate();
-    const handlesubmit=async (e)=>{
-        e.preventDefault(); 
-        const response = await fetch("http://localhost:5000/api/auth/login", {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({email:cerdential.email,password:cerdential.password})
-          });
-          const json= await response.json();
-          console.log(json);
-          if(json.success){
-            // Save the auth token and redirect 
-            localStorage.setItem('token',json.authtoken)
-            navigate('/')
-            props.showAlert("Logged in Successfully","success")
-          }else{
-            props.showAlert("Invalid Details","danger")
-          }
+  const [cerdential, setCerdential] = useState({ email: "", password: "" });
+  let navigate = useNavigate();
+  const handlesubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: cerdential.email, password: cerdential.password })
+    });
+    const json = await response.json();
+    console.log(json);
+    if (json.success) {
+      // Save the auth token and redirect 
+      localStorage.setItem('token', json.authtoken)
+      navigate('/')
+      props.showAlert("Logged in Successfully", "success")
+    } else {
+      props.showAlert("Invalid Details", "danger")
     }
-    const onChange=(e)=>{
-        setCerdential({...cerdential,[e.target.name]:e.target.value})
-    }
+  }
+  const onChange = (e) => {
+    setCerdential({ ...cerdential, [e.target.name]: e.target.value })
+  }
 
   return (
     <div className='container'>
       <h2 className='mb-3'>Login to iNoteBook</h2>
       <form onSubmit={handlesubmit}>
-  <div className="mb-3">
-    <label htmlFor="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" name='email' id="email" aria-describedby="emailHelp" value={cerdential.email} onChange={onChange}/>
-  </div>
-  <div className="mb-3">
-    <label htmlFor="password" className="form-label">Password</label>
-    <input type="password" className="form-control" name='password' id="password" value={cerdential.password} onChange={onChange}/>
-  </div>
-  <button type="submit" className="btn btn-dark">Submit</button>
-</form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" name='email' id="email" aria-describedby="emailHelp" value={cerdential.email} onChange={onChange} />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" name='password' id="password" value={cerdential.password} onChange={onChange} />
+        </div>
+        <button type="submit" className="btn btn-dark">Submit</button>
+      </form>
     </div>
   );
 }
